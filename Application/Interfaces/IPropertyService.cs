d189f8fb09fdd001d844bcbef60b2609348ddcea
Yohansel Mieses <miesesyohansel@gmail.com>
2025-12-09 23:47:30 -0400

    Los Unit Test apa, Alla traaaaa

--- Application/Interfaces/IPropertyService.cs
+++ Application/Interfaces/IPropertyService.cs
@@ -6,12 +6,11 @@ namespace Application.Services
     public interface IPropertyService : IGenericService<PropertyDto>
     {
         Task<PropertyDto?> AddPropertyAsync(CreatePropertyDto dto);
-        Task<List<PropertyDto>> GetProperties(string agentId, bool onlyAvailable);
+        Task<List<PropertyDto>> GetProperties(string agentId, bool onlyAvailable); // obtiene por agentes
         Task<List<PropertyDto>> GetAllProperties(bool onlyAvailable);
         Task<PropertyDto?> GetPropertyById(int id);
         Task<PropertyDto?> UpdatePropertyAsync(int id, PropertyDto dto);
         Task<bool> DeletePropertyAsync(int id);
-
         Task<List<PropertyDto>> GetAvailablePropertiesAsync();
 
     }
--- RealEstateApi/Program.cs
+++ RealEstateApi/Program.cs
@@ -1,8 +1,8 @@
 
 namespace RealEstateApi
 {
-    public class Program
-    {
+    public class Program 
+    { 
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
